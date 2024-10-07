46507ce8a92b3bd6a73d1ceee8fe54f2fc0c8cd0
Solovey Roman <gasoss221510@gmail.com>
2024-10-07 21:22:42 +0300

    chore: delete useless import in jwt.strategy.ts

--- src/user/strategies/jwt.strategy.ts
+++ src/user/strategies/jwt.strategy.ts
@@ -1,10 +1,4 @@
-import {
-  HttpException,
-  HttpStatus,
-  Inject,
-  Injectable,
-  Logger,
-} from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
@@ -20,7 +14,6 @@ export const JWT_STRATEGY_NAME = 'jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  private readonly logger = new Logger(JwtStrategy.name);
   constructor(
     private readonly configService: ConfigService,
     @Inject(AUTH_SERVICE_TAG) private client: ClientProxy,
