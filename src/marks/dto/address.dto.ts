8447ddf04b920f5a8e2eeaabfdb6455f972de5a7
Solovey Roman <gasoss221510@gmail.com>
2024-12-09 23:26:24 +0300

    docs: add swagger decorators to address.dto

--- src/marks/dto/address.dto.ts
+++ src/marks/dto/address.dto.ts
@@ -1,10 +1,13 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AddressDto {
+  @ApiProperty({ description: 'Описание адреса', example: 'Страна, Город' })
   @IsNotEmpty()
   @IsString()
   description: string;
 
+  @ApiProperty({ description: 'Имя адреса', example: 'Улица, дом 12' })
   @IsNotEmpty()
   @IsString()
   name: string;
